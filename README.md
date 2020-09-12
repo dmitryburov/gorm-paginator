c1ebed927a947c9f986178542560b229900f380e
Dmitry Burov <dim0n1987@yandex.ru>
2020-09-12 13:56:52 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# GORM V2 paginator
+
+A paginator doing cursor-based pagination based on [GORM v2](https://github.com/go-gorm/gorm)
+
+> Please, checkout release and docs, for introduces some incompatible-API change and many improvements: <br>[GORM 2.0 Release Note](https://gorm.io/docs/v2_release_note.html)
+
+<br>
+
+## Installation
+
+```sh
+go get -u github.com/dmitryburov/gorm-paginator
+```
+<br>
+
+## Usage
+
+- GORM Guides: https://gorm.io
+
+```go
+type Book struct {
+	gorm.Model
+	Title string
+}
+
+var (
+    dbEntity = db
+    paging   = paginator.Paging{}
+    bookList = struct {
+        Items      []*Book
+        Pagination *paginator.Pagination
+    }{}
+)
+
+// change paging params from query data
+// if len(query.Get("limit")) > 0 && query.Get("limit") != "" {
+//     paging.Limit, _ = strconv.Atoi(query.Get("limit"))
+// }
+ 
+// change DB filters and ect.
+// dbEntity.Where("id > ?", 1)
+
+bookList.Pagination, err := paginator.Pages(&paginator.Param{
+    DB:     dbEntity,
+    Paging: &paging,
+}, &bookList.Items)
+if err != nil {
+    log.Fatal("Error get list: ", err.Error())
+}
+
+fmt.Printf("%+v\n", booklist.Items)      // result data
+fmt.Printf("%+v\n", booklist.Pagination) // result pagination
+
+```
+
+<br>
+
+## License
+
+[MIT](LICENSE)
---
+++ paginator.go
@@ -0,0 +1,110 @@
+package paginator
+
+import (
+	"gorm.io/gorm"
+	"math"
+)
+
+type Pagination struct {
+	TotalRecords int64 `json:"totalRecords"`
+	TotalPage    int   `json:"totalPage"`
+	Offset       int   `json:"offset"`
+	Limit        int   `json:"limit"`
+	Page         int   `json:"page"`
+	PrevPage     int   `json:"prevPage"`
+	NextPage     int   `json:"nextPage"`
+}
+
+type Paging struct {
+	Page    int      `json:"page"`
+	OrderBy []string `json:"orderBy"`
+	Limit   int      `json:"limit"`
+	ShowSQL bool
+}
+
+type Param struct {
+	DB     *gorm.DB
+	Paging *Paging
+}
+
+// Endpoint for pagination
+func Pages(p *Param, result interface{}) (paginator *Pagination, err error) {
+
+	var (
+		done     = make(chan bool, 1)
+		db       = p.DB
+		defPage  = 1
+		defLimit = 20
+		count    int64
+		offset   int
+	)
+
+	// get all counts
+	go getCounts(db, result, done, &count)
+
+	// if not defined
+	if p.Paging == nil {
+		p.Paging = &Paging{}
+	}
+
+	// debug sql
+	if p.Paging.ShowSQL {
+		db = db.Debug()
+	}
+	// limit
+	if p.Paging.Limit == 0 {
+		p.Paging.Limit = defLimit
+	}
+	// page
+	if p.Paging.Page < 1 {
+		p.Paging.Page = defPage
+	} else if p.Paging.Page > 1 {
+		offset = (p.Paging.Page - 1) * p.Paging.Limit
+	}
+	// sort
+	if len(p.Paging.OrderBy) > 0 {
+		for _, o := range p.Paging.OrderBy {
+			db = db.Order(o)
+		}
+	} else {
+		str := "id desc"
+		p.Paging.OrderBy = append(p.Paging.OrderBy, str)
+	}
+
+	// get
+	res := db.Limit(p.Paging.Limit).Offset(offset).Find(result)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	<-done
+
+	// total pages
+	total := int(math.Ceil(float64(count) / float64(p.Paging.Limit)))
+
+	// construct pagination
+	paginator = &Pagination{
+		TotalRecords: count,
+		Page:         p.Paging.Page,
+		Offset:       offset,
+		Limit:        p.Paging.Limit,
+		TotalPage:    total,
+		PrevPage:     p.Paging.Page,
+		NextPage:     p.Paging.Page,
+	}
+
+	// prev page
+	if p.Paging.Page > 1 {
+		paginator.PrevPage = p.Paging.Page - 1
+	}
+	// next page
+	if p.Paging.Page != paginator.TotalPage {
+		paginator.NextPage = p.Paging.Page + 1
+	}
+
+	return paginator, nil
+}
+
+func getCounts(db *gorm.DB, anyType interface{}, done chan bool, count *int64) {
+	db.Model(anyType).Count(count)
+	done <- true
+}
